432f977c83b30896c9b77801bd6a6e0279034b5e
thaivanngoc1405 <ngocthaivan1405@gmail.com>
2025-05-09 08:55:11 +0700

    Delete bài 4 directory

--- bài 4/bài 4.14.py
+++
@@ -1,8 +0,0 @@
-print("Thai Van Ngoc ")
-print("MSSV:235752020710016 ")
-print("#####################")
-ds = input("Nhập chuỗi: ").split()
-ds = [element.strip() for element in ds]
-sorted_list = sorted(ds)
-print("Danh sách đã sắp xếp:", sorted_list)
-
