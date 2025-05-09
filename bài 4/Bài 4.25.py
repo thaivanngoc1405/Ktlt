96035e80c5898adefbb3d9388a27d8933fdd32db
thaivanngoc1405 <ngocthaivan1405@gmail.com>
2025-05-09 08:57:59 +0700

    Delete bài 4 directory

--- bài 4/Bài 4.25.py
+++
@@ -1,7 +0,0 @@
-print("Thai Van Ngoc ")
-print("MSSV:235752020710016 ")
-print("#####################")
-input_list = input("Nhập vào danh sách các số, cách nhau bằng dấu phẩy: ").split(',')
-input_list = [int(x) for x in input_list]
-odd_numbers = [x for x in input_list if x % 2 != 0]
-print("Các số lẻ từ danh sách là:", odd_numbers)
