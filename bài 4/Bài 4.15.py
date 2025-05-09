061a39c72b90a08c886e0ffd521db6bbf257cf20
thaivanngoc1405 <ngocthaivan1405@gmail.com>
2025-05-09 09:01:13 +0700

    Delete bài 4 directory

--- bài 4/Bài 4.15.py
+++
@@ -1,9 +0,0 @@
-print("Thai Van Ngoc ")
-print("MSSV:235752020710016 ")
-print("#####################")
-input_str = input("Nhập chuỗi các từ tiếng Anh: ")
-words = input_str.split()
-words.sort()
-print("Các từ theo thứ tự từ điển là:")
-for word in words:
-    print(word)
--- bài 4/Bài 4.9.py
+++
@@ -1,4 +0,0 @@
-print("Thai Van Ngoc ")
-print("MSSV: 235752020710016")
-print("#####################")
-ds = input('Nhập chuỗi:').split()
